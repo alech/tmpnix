80d8ff10088f27bbf25c16d891a924c3566a3c46
Alexander Klink <git@alech.de>
2019-02-04 17:22:47 +0100

    search option for tmpnix.py, documentation on how to untar the tarball

--- README.md
+++ README.md
@@ -65,8 +65,10 @@ docker run -it --mount source=tmpnix,destination=/tmp/nix tmpnix build nixpkgs.s
 # After finishing to build, the script will tell you how to copy the
 # tar-ball containing the build result and all of its run-time
 # dependencies from the container.
+# Once you copied the container, use tar xjf <tarball> -C / to unpack with
+# / as a destination
 
-# If you want to search for packages, you can use the standard
-# Nix commands, e.g.
-docker run tmpnix nix-env -qaP '.*socat.*'
+# If you want to search for packages, you can use the search
+# commands, e.g.
+docker run tmpnix search '.*socat.*'
 ```
--- base/tmpnix.py
+++ base/tmpnix.py
@@ -5,53 +5,62 @@ import subprocess
 import os
 
 def usage():
-    print("Usage: build packagename")
+    print("Usage: build packagename | search '.*package.*'")
     sys.exit(1)
 
+def build(package):
+    res = subprocess.run(["nix-env", "-iA", "--dry-run", package])
+    if res.returncode != 0:
+        print("Installer dry run failed, are you sure the package name is correct?");
+        print("Try searching for packages with nix-env -qaP '.*name.*'")
+        sys.exit(2)
+
+    # start building
+    try:
+        subprocess.call(["nix-env", "-iA", package])
+    except subprocess.CalledProcessError:
+        print("Build failed :(, investigate or try to update channel with")
+        print("nix-channel --update inside the container")
+        sys.exit(3)
+
+    # get the name for the tarball
+
+    res = subprocess.run(["nix-env", "--query", "-a", "-A", package], stdout=subprocess.PIPE)
+    name = res.stdout.decode("ascii").strip()
+
+    res = subprocess.run(["nix-env", "--query", "-a", "--no-name", "--out-path", "-A", package], stdout=subprocess.PIPE)
+    outpath = res.stdout.decode("ascii").strip()
+
+    res = subprocess.run(["nix-store", "--query", "--requisites", outpath], stdout=subprocess.PIPE)
+    dirs_to_tar = res.stdout.decode("ascii").strip().split("\n")
+
+    hostname = open('/etc/hostname', 'r').read().strip()
+    buildprefix = open('/home/tmpnix/.buildprefix').read().strip()
+
+    package_path = os.path.join(buildprefix, "tmpnix-packages")
+    if not os.path.isdir(package_path):
+        os.mkdir(package_path)
+
+    tar_name = package_path + "/tmpnix-" + name + ".tar.bz2"
+    subprocess.run(["tar", "cjf", tar_name] + dirs_to_tar)
+
+    print("We are done. \o/")
+    print("Pick up your tarball here:")
+    print("docker cp " + hostname + ":" + tar_name + " .")
+
+def search(package):
+    subprocess.run(["nix-env", "-qaP", package])
+
+# MAIN
 if len(sys.argv) != 3:
     usage()
 
 command = sys.argv[1]
 package = sys.argv[2]
 
-if command != "build":
+if command == "build":
+    build(package)
+elif command == "search":
+    search(package)
+else:
     usage()
-
-res = subprocess.run(["nix-env", "-iA", "--dry-run", package])
-if res.returncode != 0:
-    print("Installer dry run failed, are you sure the package name is correct?");
-    print("Try searching for packages with nix-env -qaP '.*name.*'")
-    sys.exit(2)
-
-# start building
-try:
-    subprocess.call(["nix-env", "-iA", package])
-except subprocess.CalledProcessError:
-    print("Build failed :(, investigate or try to update channel with")
-    print("nix-channel --update inside the container")
-    sys.exit(3)
-
-# get the name for the tarball
-
-res = subprocess.run(["nix-env", "--query", "-a", "-A", package], stdout=subprocess.PIPE)
-name = res.stdout.decode("ascii").strip()
-
-res = subprocess.run(["nix-env", "--query", "-a", "--no-name", "--out-path", "-A", package], stdout=subprocess.PIPE)
-outpath = res.stdout.decode("ascii").strip()
-
-res = subprocess.run(["nix-store", "--query", "--requisites", outpath], stdout=subprocess.PIPE)
-dirs_to_tar = res.stdout.decode("ascii").strip().split("\n")
-
-hostname = open('/etc/hostname', 'r').read().strip()
-buildprefix = open('/home/tmpnix/.buildprefix').read().strip()
-
-package_path = os.path.join(buildprefix, "tmpnix-packages")
-if not os.path.isdir(package_path):
-    os.mkdir(package_path)
-
-tar_name = package_path + "/tmpnix-" + name + ".tar.bz2"
-subprocess.run(["tar", "cjf", tar_name] + dirs_to_tar)
-
-print("We are done. \o/")
-print("Pick up your tarball here:")
-print("docker cp " + hostname + ":" + tar_name + " .")
